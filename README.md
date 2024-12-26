7a59d4bc730baff3b181ca8e00093535322b32d6
ManuCoutinho <gdn.sasha@gmail.com>
2024-12-26 19:44:28 -0300

    :books: add swagger setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Octo Shop Api
+
+### :construction_worker_woman: Work in progress :construction_worker_woman:
+
+To start this app follow the instructions below:
+
+```bash
+$ docker file coming soon
+```
+### :books: Documentation
+
+Api documentation available in: `localhost:8080/swagger`
--- src/main/java/com/evergarden/crud_basic/resources/UserResource.java
+++ src/main/java/com/evergarden/crud_basic/resources/UserResource.java
@@ -2,6 +2,9 @@ package com.evergarden.crud_basic.resources;
 
 import com.evergarden.crud_basic.entities.User;
 import com.evergarden.crud_basic.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +16,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
+@Tag(name = "user", description = "user management")
 public class UserResource {
 	@Autowired
 	private UserService service;
 	@GetMapping
+	@Operation(summary = "Retrieve list of all users", description = "Get all users")
+	@ApiResponse(responseCode = "200", description = "Success")
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
